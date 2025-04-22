22f96f102b22b709ba87443d92d1e6d430db1196
Kaiyuan Han <hermit_han@163.com>
2025-04-22 14:45:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# taf-tgsgapcloser
+
+- This is a taf-app(taf-tool), you can use taffish(https://www.taffish.com) to use this taf-app.
+- This app is from https://github.com/BGI-Qingdao/TGS-GapCloser
+
+A gap-closing software tool that uses long reads to enhance genome assembly.
---
+++ tgsgapcloser.info.lisp
@@ -0,0 +1,7 @@
+(:name           "tgsgapcloser")
+(:repo           "taffish-org/tgsgapcloser")
+(:author         "KaiyuanHan")
+(:version        "1.2.1")
+(:intro          "A gap-closing software tool that uses long reads to enhance genome assembly.")
+(:dependencies)
+(:functions      "tgsgapcloser")
